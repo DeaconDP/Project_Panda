b7dd513e6cfc7f4f30d1fe3c35e1c015993ae979
TheyCallHimMrBojangles <deacondp@gmail.com>
2019-05-31 11:31:12 +0200

    Initial Version 0.4
    Added Project to GitHub

---
+++ Assets/camera_script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_script : MonoBehaviour
+{
+    public int playerSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButton("fire1"))
+        { 
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+        
+    }
+}
---
+++ Assets/movementscript2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementscript2 : MonoBehaviour
+
+{
+public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed=1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
